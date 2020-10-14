dacf8e8bd8a092bc2b62e7c55b55b1b8aee049fb
RainbowJM <Jevona98@live.com>
2020-10-14 14:38:26 +0200

    AdresDAOHibernate Done

--- src/main/java/nl/hu/dp/ovchip/dao/AdresDAOHibernate.java
+++ src/main/java/nl/hu/dp/ovchip/dao/AdresDAOHibernate.java
@@ -4,60 +4,96 @@ import nl.hu.dp.ovchip.domein.Adres;
 import nl.hu.dp.ovchip.domein.Reiziger;
 import nl.hu.dp.ovchip.interfaces.AdresDAO;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class AdresDAOHibernate implements AdresDAO {
-    private SessionFactory factory;
+    private Session session;
+    private AdresDAO adao;
 
-    public AdresDAOHibernate(SessionFactory factory){
-        this.factory = factory;
+    public AdresDAOHibernate(Session session){
+        this.session = session;
     }
 
+    public void setAdao(AdresDAO adao){
+        this.adao = adao;
+    }
     @Override
     public boolean save(Adres adres) {
-        Session session = factory.openSession();
-
-        Transaction trans = session.beginTransaction();
-        session.save(adres);
-        trans.commit();
-
-        return true;
+        try {
+            Transaction trans = session.beginTransaction();
+            session.save(adres);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean update(Adres adres) {
-        Session session = factory.openSession();
-        session.update(adres);
-        return true;
+        try{
+            Transaction trans = session.beginTransaction();
+            session.update(adres);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean delete(Adres adres) {
-        Session session = factory.openSession();
-        session.delete(adres);
-        return true;
+        try{
+            Transaction trans = session.beginTransaction();
+            session.delete(adres);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public Adres findById(int id) {
-        Session session = factory.openSession();
-        Adres adres = session.get(Adres.class, id);
-        return adres;
+        try{
+            Transaction trans = session.beginTransaction();
+            Adres adres = session.get(Adres.class, id);
+            trans.commit();
+            return adres;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public Adres findByReiziger(Reiziger reiziger) {
-        Session session = factory.openSession();
-        Adres adres = session.get(Adres.class, reiziger.getReiziger_id());
-        return adres;
+        try{
+            Transaction trans = session.beginTransaction();
+            Adres adres = session.get(Adres.class, reiziger.getReiziger_id());
+            trans.commit();
+            return adres;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<Adres> findAll() {
-//        List<Adres>
-        return null;
+        try{
+            Transaction trans = session.beginTransaction();
+            List<Adres> adresList = session.createQuery("FROM adres").list();
+            trans.commit();
+            return adresList;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
