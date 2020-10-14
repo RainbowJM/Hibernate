1bf5e060e33cd69c0e36e2c48778ba2232074395
RainbowJM <Jevona98@live.com>
2020-10-14 16:22:13 +0200

    ReizigerDAOHibernate Done

--- src/main/java/nl/hu/dp/ovchip/Main.java
+++ src/main/java/nl/hu/dp/ovchip/Main.java
@@ -1,9 +1,17 @@
 package nl.hu.dp.ovchip;
 
+import nl.hu.dp.ovchip.dao.AdresDAOHibernate;
+import nl.hu.dp.ovchip.dao.OvChipkaartDAOHibernate;
+import nl.hu.dp.ovchip.dao.ProductDAOHibernate;
+import nl.hu.dp.ovchip.dao.ReizigerDAOHibernate;
 import nl.hu.dp.ovchip.domein.Adres;
 import nl.hu.dp.ovchip.domein.OvChipkaart;
 import nl.hu.dp.ovchip.domein.Product;
 import nl.hu.dp.ovchip.domein.Reiziger;
+import nl.hu.dp.ovchip.interfaces.AdresDAO;
+import nl.hu.dp.ovchip.interfaces.OvChipkaartDAO;
+import nl.hu.dp.ovchip.interfaces.ProductDAO;
+import nl.hu.dp.ovchip.interfaces.ReizigerDAO;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -49,6 +57,23 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         testFetchAll();
+
+        //DAO
+        ReizigerDAOHibernate rdao = new ReizigerDAOHibernate(getSession());
+        AdresDAOHibernate adao = new AdresDAOHibernate(getSession());
+//        OvChipkaartDAOHibernate odao = new OvChipkaartDAOHibernate(getSession());
+//        ProductDAOHibernate pdao = new ProductDAOHibernate(getSession());
+
+
+        //relation
+        rdao.setAdao(adao);
+//        rdao.setOdao(odao);
+//        odao.setRdao(rdao);
+//        odao.setPdao(pdao);
+//        pdao.setOdao(odao);
+
+        testDAOHibernate(rdao);
+    //,adao,odao,pdao);
     }
 
     /**
@@ -67,6 +92,65 @@ public class Main {
                 }
                 System.out.println();
             }
+        } finally {
+            session.close();
+        }
+    }
+
+    private static  void testDAOHibernate(ReizigerDAO rdao){//, AdresDAO adao, OvChipkaartDAO odao, ProductDAO pdao){
+        Session session = getSession();
+        try{
+            System.out.println("\n---------- Test ReizigerDAO -------------");
+
+            // Haal alle reizigers op uit de database
+            List<Reiziger> reizigers = rdao.findAll();
+            System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+            for (Reiziger r : reizigers) {
+                System.out.println(r);
+            }
+            System.out.println("------");
+
+            // Maak een nieuwe reizigers aan en persisteer deze in de database
+            String gbdatum1 = "1981-03-14";
+//            Reiziger sietske = new Reiziger(8, "S", "", "Boers", java.sql.Date.valueOf(gbdatum1));
+//            rdao.save(sietske);
+//             xrdao.delete(sietske);
+
+            String gbdatum2 = "2000-07-22";
+            Reiziger ellen = new Reiziger(9, "E", "L", "Lopez", java.sql.Date.valueOf(gbdatum2));
+            rdao.save(ellen);
+//        rdao.delete(ellen);
+
+            System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+            System.out.println("\n----------------------------");
+
+            // Haal alle reizigers op uit de database
+            List<Reiziger> nReizigers = rdao.findAll();
+            System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+            for (Reiziger r : nReizigers) {
+                System.out.println(r);
+            }
+            System.out.println();
+
+//            reizigers = rdao.findAll();
+//            System.out.println(reizigers.size() + " reizigers\n");
+
+            // Delete de net aangemaakt reiziger
+            rdao.delete(ellen);
+            reizigers = rdao.findAll();
+            System.out.println(reizigers.size() + " reizigers na het verwijderen van de net aangemaakt reiziger\n");
+            System.out.println("\n----------------------------");
+
+            // Find by id
+            Reiziger reizigerId = rdao.findById(1);
+            System.out.println("[Test] ReizigerDAO.findById() geeft: " + reizigerId);
+            System.out.println();
+            System.out.println("\n----------------------------");
+
+            // Find by birthday
+            List<Reiziger> reizigerG = rdao.findByGbdatum(gbdatum1);
+            System.out.println("[Test] ReizigerDAo.findByGbdatum() geeft \n" + reizigerG);
+
         } finally {
             session.close();
         }
--- src/main/java/nl/hu/dp/ovchip/dao/ReizigerDAOHibernate.java
+++ src/main/java/nl/hu/dp/ovchip/dao/ReizigerDAOHibernate.java
@@ -1,54 +1,99 @@
 package nl.hu.dp.ovchip.dao;
 
+import nl.hu.dp.ovchip.domein.Adres;
 import nl.hu.dp.ovchip.domein.Reiziger;
+import nl.hu.dp.ovchip.interfaces.AdresDAO;
+import nl.hu.dp.ovchip.interfaces.OvChipkaartDAO;
 import nl.hu.dp.ovchip.interfaces.ReizigerDAO;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.sql.Date;
 import java.util.List;
 
 public class ReizigerDAOHibernate implements ReizigerDAO {
-    private SessionFactory factory;
+    private Session session;
+    private AdresDAO adao;
+    private OvChipkaartDAO odao;
 
-    public ReizigerDAOHibernate(SessionFactory factory){
-        this.factory = factory;
+    public ReizigerDAOHibernate(Session session){
+        this.session = session;
     }
+
+    public void setAdao(AdresDAO adao){this.adao = adao;}
+
+    public void setOdao(OvChipkaartDAO odao){this.odao = odao;}
 
     @Override
     public boolean save(Reiziger reiziger) {
-        Session session = factory.openSession();
-        session.save(reiziger);
-        return true;
+        try {
+            Transaction trans = session.beginTransaction();
+            session.save(reiziger);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean update(Reiziger reiziger) {
-        Session session = factory.openSession();
-        session.update(reiziger);
-        return true;
+        try{
+            Transaction trans = session.beginTransaction();
+            session.update(reiziger);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean delete(Reiziger reiziger) {
-        Session session = factory.openSession();
-        session.delete(reiziger);
-        return true;
+        try{
+            Transaction trans = session.beginTransaction();
+            session.delete(reiziger);
+            trans.commit();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public Reiziger findById(int id) {
-        Session session = factory.openSession();
-        Reiziger reiziger = session.get(Reiziger.class, id);
-        return reiziger;
+        try{
+            Reiziger reiziger = session.get(Reiziger.class, id);
+            return reiziger;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<Reiziger> findByGbdatum(String datum) {
-        return null;
+        try{
+            Date date = java.sql.Date.valueOf(datum);
+            List<Reiziger> reizigerList = session.createQuery("FROM reiziger WHERE geboortedatum = '" + date+"'",Reiziger.class).getResultList();
+            return reizigerList;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<Reiziger> findAll() {
-        return null;
+        try{
+            List<Reiziger> reizigerList = session.createQuery("FROM reiziger",Reiziger.class).getResultList();
+            return reizigerList;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
